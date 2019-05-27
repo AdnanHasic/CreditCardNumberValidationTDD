53c702eb35a8f12f9de6ccf6ea79906a980db05c
Adnan Hasic <hasic.adnan.ado@gmail.com>
2019-05-27 23:23:19 +0200

    kreirana test metoda i metodad za sumu cifara na parnim pozicijama pomnozenim sa 2

--- src/CreditCardNumberValidation.java
+++ src/CreditCardNumberValidation.java
@@ -1,2 +1,27 @@
 public class CreditCardNumberValidation {
+
+
+    public int sumOfDoubleEvenPlace(long creditCardNumber) {
+
+        int  sumOfDoubleEvenPlace = 0;
+
+       String creditCardnNumberToString = Long.toString(creditCardNumber);
+
+       for(int i =(creditCardnNumberToString.length() - 2); i >= 0; i -= 2) {
+
+           int doubleNumber = (Integer.parseInt(Character.toString(creditCardnNumberToString.charAt(i))) * 2);
+
+           if(doubleNumber > 9) {
+
+               int nubmer1 = doubleNumber / 10;
+               int number2 = doubleNumber % 10;
+               doubleNumber = nubmer1 + number2;
+           }
+
+           sumOfDoubleEvenPlace += doubleNumber;
+
+       }
+
+        return sumOfDoubleEvenPlace;
+    }
 }
--- src/CreditCardNumberValidationTest.java
+++ src/CreditCardNumberValidationTest.java
@@ -1,22 +1,38 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class CreditCardNumberValidationTest {
 
     CreditCardNumberValidation card1;
+    long creditCardNumber;
 
     @BeforeEach
     void setUp() {
 
+
         card1 = new CreditCardNumberValidation();
+        creditCardNumber = 512345678901234L;
 
     }
 
     @Test
     public void isValidTest_ShouldReturnTrue_IfCreditCardNumberIsValid() {
 
-        
+
     }
+
+    @Test
+    public void sumOfDoubleEvenPlaceTest_ShouldReturnSumOfDoubleEvenPlace_IfGivenCreditCardNumber(){
+
+        int result = card1.sumOfDoubleEvenPlace(creditCardNumber);
+
+        int expected = 31;
+
+        Assertions.assertEquals(expected,result);
+
+    }
+
+    
+
 }
