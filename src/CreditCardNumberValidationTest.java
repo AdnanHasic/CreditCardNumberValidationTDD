020e84cb882e33df20ca9178d1807cf426f47f0c
Adnan Hasic <hasic.adnan.ado@gmail.com>
2019-05-27 17:52:09 +0200

    kreirana CreditCardNumberValidation i CreditCardNumberValidationTest klase

---
+++ src/CreditCardNumberValidationTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreditCardNumberValidationTest {
+
+    CreditCardNumberValidation card1;
+
+    @BeforeEach
+    void setUp() {
+
+        card1 = new CreditCardNumberValidation();
+
+    }
+
+    @Test
+    public void isValidTest_ShouldReturnTrue_IfCreditCardNumberIsValid() {
+
+        
+    }
+}
